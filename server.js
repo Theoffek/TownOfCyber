5497404aa4c52cea3d085de191d511a5703af943
The offek <offek.alon@gmail.com>
2020-04-08 00:19:48 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const {
     getRoomNames
 } = require('./public/utils/users');
 
+let gameStates = {}
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -23,12 +25,25 @@ const botName = 'Alex';
 
 let messageLog = []
 
+const messageLogFile = "../logs/message-log.json"
+const fs = require("fs")
+
+fs.mkdir("../logs", err => {})
+
 setInterval(() => {
-    let string = JSON.stringify(messageLog)
-    messageLog = [];
-    const fs = require("fs");
-    //fs.writeFile("log-" + Date.now() + ".json", string, err => console.log("failed to save messagelog"))
-}, 1000 * 60 * 1)
+    fs.readFile(messageLogFile, (err, data) => {
+        let writtenLog = []
+        if (err) {
+            console.log(err)
+        } else {
+            fs.unlinkSync(messageLogFile);
+            writtenLog = JSON.parse(data);
+        }
+
+        fs.writeFileSync(messageLogFile, JSON.stringify(writtenLog.concat(messageLog), null, 2))
+        messageLog = []
+    })
+}, 1000 * 20 * 1)
 
 // Run when client connects
 io.on('connection', socket => {
@@ -48,13 +63,13 @@ io.on('connection', socket => {
             .to(user.room)
             .emit(
                 'message',
-                formatMessage(botName, `${user.username} has joined the chat`)
+                formatMessage(botName, `${user.username} Has joined the chat`)
             );
 
         // Send users and room info
         io.to(user.room).emit('roomUsers', {
             room: user.room,
-            myRole: user.role, //idk what im doing
+            myRole: user.role,
             users: getRoomUsers(user.room)
         });
     });
@@ -63,13 +78,54 @@ io.on('connection', socket => {
     socket.on('chatMessage', msg => {
         const user = getCurrentUser(socket.id);
 
+        if (user == null) return;
+
         // save message to log...
         messageLog.push({
                 "user": user,
                 "msg": msg
             })
             // commands and /start (starting game) 
-        if (msg.startsWith("/")) {
+        if (msg == "/start") {
+            essentialRoles = ["peasant", "peasant", "peasant", "sheriff", "mafia", "mafia"];
+            otherRoles = ["medic", "veteran", "vigilante", "medic", "mafia", "serial killer", "peasant", "medic", "peasant", "peasant"];
+            users = getRoomUsers(user.room);
+            if (users.length < 6) {
+                socket.emit('message', formatMessage(botName, 'ERROR: user number less than 6.'))
+            } else {
+                if (users.length > 16) {
+                    socket.emit('message', formatMessage(botName, 'ERROR: too many users (max 16).'))
+                } else {
+                    shuffledUsers = users.sort(() => Math.random() - 0.5);
+                    shuffledRoles = [
+                        ...essentialRoles.sort(() => Math.random() - 0.5),
+                        ...otherRoles.sort(() => Math.random - 0.5)
+                    ];
+                    for (let i = 0; i < shuffledUsers.length; i++) {
+                        shuffledUsers[i].role = shuffledRoles[i];
+                    }
+
+                    let nightTimer, dayTimer
+
+                    gameStates[room] = 'day'
+                    setTimout(() => nightTimer = setInterval(function() { gameState[room] = 'night' }, 60000), 30000);
+                    dayTimer = setInterval(function() { gameState[room] = 'day' }, 60000);
+                    socket.emit('message', formatMessage(botName, 'Starting the game!'));
+                    while (gameSates[room] != null) {
+                        while (gameStates[room] == day) {
+
+                        }
+                        while (gameStates[room] == night) {
+
+                        }
+
+                    }
+                }
+            }
+        } else if (msg.startsWith("/")) {
+            // don't do anything if the gamestate is null or undefined
+            if (gameStates[user.room] == null) return;
+
             let command = msg.substring(1).split(/[ \t]+/);
 
             if (command[0] == "w") {
@@ -116,13 +172,13 @@ io.on('connection', socket => {
             // Send users and room info
             io.to(user.room).emit('roomUsers', {
                 room: user.room,
-                myRole: user.role, //idk what im doing
+                myRole: user.role,
                 users: getRoomUsers(user.room)
             });
         }
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
